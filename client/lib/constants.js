d70ebde137cf859481b6c78a1f228a71073f8107
Damir <damir@withpulp.com>
2015-03-22 00:02:05 -0400

    added seo config; created constants for seo and spacebar helpers

---
+++ client/lib/constants.js
@@ -0,0 +1,10 @@
+// Define App Constants
+
+if (Meteor.App) {
+  throw new Meteor.Error('Meteor.App already defined? see client/lib/constants.js');
+}
+
+Meteor.App = {
+  NAME: 'Landing Page Builder',
+  DESCRIPTION: 'Landing Page Builder app built with Meteor.'
+};

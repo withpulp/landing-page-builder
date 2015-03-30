281eab01d915d0a2f8a336293d9955d76dc20faa
Damir <damir@withpulp.com>
2015-03-30 16:47:43 -0400

    mods to editor and settings, hardcoded admin username into fixtures - wip

--- server/fixtures/contents.js
+++ server/fixtures/contents.js
@@ -1,7 +1,7 @@
 Fixtures = typeof Fixtures !== "undefined" ? Fixtures : {};
 
 var now = Date.now(),
-    admin = Meteor.users.find({username: 'admin'});
+    admin = 'admin';
 
 Fixtures.contents = [
   {
@@ -10,10 +10,13 @@ Fixtures.contents = [
     heroTitle: 'Hero Title',
     heroContent: 'This is the hero content',
     heroImage: 'http://vignette1.wikia.nocookie.net/berserk/images/8/8c/Berserker_Armour_Version_2.png/revision/latest?cb=20120517182924',
-    content: '##Hello World!\n\nWrite your content with markdown.',
+    newsletter: true,
+    mailchimpAPI: 'xxxxxxxxxxxxxxxxxxxxxxx-xxx',
+    mailchimpID: 'xxxxxxxxxx',
+    content: '##Hello!\n\nWrite your content with markdown.',
     date: now,
     updated: now,
-    author: admin.username,
+    author: admin,
     contentCall: function() {
       return this.content;
     }

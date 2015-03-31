9f63f527d3868836b00ad4edd8225bee7f3f597f
Damir <damir@withpulp.com>
2015-03-30 23:01:14 -0400

    added save event to menu button - content is now editable

--- client/views/admin/editor.js
+++ client/views/admin/editor.js
@@ -6,48 +6,6 @@ Template.editor.rendered = function() {
   }
 
 	var $editor = $('textarea.content');
-	
+
 	$editor.elastic();
 };
-
-Template['editor'].events({
-	'click .create.button': function(e) {
-		e.preventDefault();
-		var now = Date.now();
-
-		var newContent = {
-			content: $('.markdown .create.content').val(),
-			date: now,
-			updated: now,
-			author: Meteor.userId(),
-			contentCall: function() {
-				return this.content;
-			}
-		};
-
-		Contents.insert(newContent);
-		Router.go('home');
-		FlashMessages.sendSuccess('Content created');
-	},
-	'click .edit.button': function(e) {
-		e.preventDefault();
-
-		var now = Date.now();
-
-    var thisContentId = this._id,
-		    contentEntry = {
-          content: $('.markdown .edit.content').val(),
-          updated: now
-        };
-
-    Contents.update(thisContentId, {$set: contentEntry}, function(err) {
-      if (err) {
-				FlashMessages.sendError('Error, error!');
-        console.log(err.reason);
-      } else {
-				Router.go('home');
-				FlashMessages.sendSuccess('Content updated');
-      }
-    });
-	}
-});

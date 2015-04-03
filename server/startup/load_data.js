88aa758f53b5da328ef57c5d69befebc3418f83b
Husam Machlovi <hmachlovi@gmail.com>
2015-04-03 14:25:28 -0400

    checkbox databinding

--- server/startup/load_data.js
+++ server/startup/load_data.js
@@ -24,6 +24,7 @@ Meteor.startup(function () {
       heroContent: 'His name is Mittens. This cat does a lot of dumb shit but I still love him.',
       heroImage: 'http://placekitten.com/220/300',
       newsletter: 'on',
+      overlay: '',
       mailchimpAPI: 'xxxxxxxxxxxxxxxxxxxxxxx-xxx',
       mailchimpID: 'xxxxxxxxxx',
       content: '##The story begins!\n\nHe lived, he scratched up my furniture. He ate for free and always hated when I tried to pet him. Scratched me a bunch of times. Then he grew old and died. He was the best cat ever. The end.',

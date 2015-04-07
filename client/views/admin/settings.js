f75117cbeab0463b504ca5b1dc3422b949ce37bb
Husam Machlovi <hmachlovi@gmail.com>
2015-04-07 18:52:32 -0400

    testing viewmodel

--- client/views/admin/settings.js
+++ client/views/admin/settings.js
@@ -2,9 +2,13 @@ Template.settings.rendered = function () {
   $('.ui.checkbox').checkbox();
 };
 
-//Template.settings.helpers({
-  //if($('.ui.checkbox').hasClass('checked')) {
-  //  console.log("die");
+// Template.settings.helpers({
+  // if($('.ui.checkbox').hasClass('checked')) {
+    // console.log("die");
     //PlayersList.update(selectedPlayer, {$inc: {score: 5} });
-//  }
-//});
+  // }
+// });
+
+Template.settings.viewmodel({
+  on: 'false'
+});

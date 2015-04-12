2567925fac7fffa3108bd59bbe11f111fd1782b2
Damir <damir@withpulp.com>
2015-04-11 20:20:54 -0400

    modified model schema, added autoform markup to content tab

--- models/contents.js
+++ models/contents.js
@@ -16,6 +16,43 @@ Contents.attachSchema(new SimpleSchema({
 	title: {
 		type: String,
 		label: 'Title',
+		max: 50
+	},
+	subtitle: {
+		type: String,
+		label: 'Subtitle',
 		max: 100
+	},
+	heroTitle: {
+		type: String,
+		label: 'Hero Title',
+		max: 100
+	},
+	heroContent: {
+		type: String,
+		label: 'Hero Content',
+		max: 200
+	},
+	heroImage: {
+		type: String,
+		label: 'Hero Image',
+		optional: true
+	},
+	content: {
+		type: String,
+		label: 'Landing Page Content',
+		optional: true
+	},
+	newsletter: {
+		type: Boolean,
+		label: 'Enable Newsletter'
+	},
+	mailchimpAPI: {
+		type: String,
+		label: 'Mailchimp API Key'
+	},
+	mailchimpID: {
+		type: String,
+		label: 'Mailchimp ID'
 	}
 }));

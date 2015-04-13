77eb29d3d0bc0b7fcc88502d79c636001e725abf
Damir <damir@withpulp.com>
2015-04-13 14:52:25 -0400

    added autoform hooks onSubmit psuedocode - need to restructure form to include submit button

--- client/lib/autoform_hooks.js
+++ client/lib/autoform_hooks.js
@@ -2,6 +2,11 @@ AutoForm.hooks({
 	editSettings: {
     onSubmit: function (insertDoc, updateDoc, currentDoc) {
 			console.log(currentDoc);
+			// on form submit =>
+			// if newsletter === true
+			// set newsletter session to true
+			// else
+			// set newsletter session to false
     }
   }
 });

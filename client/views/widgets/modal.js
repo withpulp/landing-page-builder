5b7bedfca0afe6b7e87d5265e9dfdcdf5974070d
Husam Machlovi <hmachlovi@gmail.com>
2015-04-19 04:11:00 -0600

    investigations and console logs, overlayTimeout

--- client/views/widgets/modal.js
+++ client/views/widgets/modal.js
@@ -4,20 +4,30 @@ Template.modal.rendered = function() {
   }).fetch().map(function(x) {
     return x.overlayTimeout;
     }), true);
+    console.log(overlayTimeoutValue);
 
   if (overlayTimeoutValue[0] === 0) {
     var time = 500;
     console.log('0');
+    setTimeout( function(){
+      $('.ui.modal').modal('show');
+    }, time);
   }
 
   else if (overlayTimeoutValue[0] === 1) {
     var time = 2000;
     console.log('1');
+    setTimeout( function(){
+      $('.ui.modal').modal('show');
+    }, time);
   }
 
   else if (overlayTimeoutValue[0] === 2) {
     var time = 5000;
     console.log('2');
+    setTimeout( function(){
+      $('.ui.modal').modal('show');
+    }, time);
   }
 
   setTimeout( function(){

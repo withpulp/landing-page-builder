2cdfff89589ce5a10ef5081e8296455861338ce1
Damir <damir@withpulp.com>
2015-04-28 12:00:03 -0400

    removed save click event from menu button - button no longer exists

--- client/views/common/menu.js
+++ client/views/common/menu.js
@@ -5,33 +5,5 @@ Template.menu.rendered = function() {
 Template['menu'].events({
 	'click .logout' : function () {
 		Meteor.logout();
-	},
-	'click .save.button': function(e) {
-		e.preventDefault();
-
-		var now = Date.now(),
-				thisContentId = this._id,
-		    contentEntry = {
-					title: $('.title .edit.title').val(),
-					subtitle: $('.sub.title .edit.sub.title').val(),
-					heroTitle: $('.hero .hero.title').val(),
-					heroContent: $('.hero .hero.content').val(),
-					heroImage: $('.settings .hero.image').val(),
-					newsletter: $('.settings .newsletter.toggle').is(':checked'),
-					mailchimpAPI: $('.settings .newsletter.api').val(),
-					mailchimpID: $('.settings .newsletter.id').val(),
-          content: $('.markdown .edit.content').val(),
-          updated: now
-        };
-
-    Contents.update(thisContentId, {$set: contentEntry}, function(err) {
-      if (err) {
-				FlashMessages.sendError('Error, error!');
-        console.log(err.reason);
-      } else {
-				Router.go('home');
-				FlashMessages.sendSuccess('Content updated');
-      }
-    });
 	}
 });

74daecdbfd7a01c46e67a66cbcb26b8b93cc8e50
Husam Machlovi <hmachlovi@gmail.com>
2015-05-19 15:38:40 -0400

    added overlayTimeout, Title and Content fields back by default

--- client/views/admin/settings.js
+++ client/views/admin/settings.js
@@ -1,7 +1,8 @@
-Template.settings.events({
+/*Template.settings.events({
   "click input.overlay": function(){
     console.log("yay");
     Session.set('newsletterChecked', !Session.get('newsletterChecked'));
     Session.get('newsletterChecked')
   }
 });
+*/

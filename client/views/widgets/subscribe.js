b5b427c17cb50f98faf7e221d954217f3b399474
Damir <damir@withpulp.com>
2015-05-28 14:01:31 -0400

    created post url with username and dc hidden field values

--- client/views/widgets/subscribe.js
+++ client/views/widgets/subscribe.js
@@ -2,16 +2,18 @@ Template['subscribe'].events({
   'submit .subscribe.form': function(e, tpl) {
 		e.preventDefault();
 
-		console.log('mailchimp form submitted');
-
-    var params = {
-      name: tpl.$('input[name=name]').val(),
-      email: tpl.$('input[name=email]').val(),
-      c: 'JSON_CALLBACK',
-      u: tpl.$('input[name=mailchimpAPI]').val(),
-      id: tpl.$('input[name=mailchimpID]').val()
-    };
+    var username = tpl.$('input[name=mailchimpUsername]').val(),
+        dc = tpl.$('input[name=mailchimpDC]').val(),
+        url = '//' + username + '.' + dc + '.list-manage.com/subscribe/post-json',
+        params = {
+          name: tpl.$('input[name=name]').val(),
+          email: tpl.$('input[name=email]').val(),
+          c: 'JSON_CALLBACK',
+          u: tpl.$('input[name=mailchimpAPI]').val(),
+          id: tpl.$('input[name=mailchimpID]').val()
+        };
 
     console.log(params);
+    console.log(url);
 	}
 });

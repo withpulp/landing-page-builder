2d0e6c1a0cb8461322d4b742166199636ae03dad
Damir <damir@withpulp.com>
2015-05-28 15:10:41 -0400

    added notes for mailchimp form todo

--- client/views/widgets/subscribe.js
+++ client/views/widgets/subscribe.js
@@ -2,6 +2,9 @@ Template['subscribe'].events({
   'submit .subscribe.form': function(e, tpl) {
 		e.preventDefault();
 
+    // @TODO: move all this into server method for security
+    // remove hidden fields from form - pull from db
+
     var username = tpl.$('input[name=mailchimpUsername]').val(),
         dc = tpl.$('input[name=mailchimpDC]').val(),
         url = '//' + username + '.' + dc + '.list-manage.com/subscribe/post-json',

6970448a6ba31412906fef636a8e0c585498b2fc
Damir <damir@withpulp.com>
2015-05-28 13:29:11 -0400

    setup params object for subscribe form fields

--- client/views/widgets/subscribe.js
+++ client/views/widgets/subscribe.js
@@ -3,5 +3,15 @@ Template['subscribe'].events({
 		e.preventDefault();
 
 		console.log('mailchimp form submitted');
+
+    var params = {
+      name: tpl.$('input[name=name]').val(),
+      email: tpl.$('input[name=email]').val(),
+      c: 'JSON_CALLBACK',
+      u: tpl.$('input[name=mailchimpAPI]').val(),
+      id: tpl.$('input[name=mailchimpID]').val()
+    };
+
+    console.log(params);
 	}
 });

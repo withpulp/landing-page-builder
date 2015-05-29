d23f4a04100f077b43be3b986129221ee2af9414
Damir <damir@withpulp.com>
2015-05-28 22:35:21 -0400

    more work on subscribe method - hardcoded some vars - wip

--- client/views/widgets/subscribe.js
+++ client/views/widgets/subscribe.js
@@ -7,13 +7,19 @@ Template['subscribe'].events({
 
     var username = tpl.$('input[name=mailchimpUsername]').val(),
         dc = tpl.$('input[name=mailchimpDC]').val(),
-        url = '//' + username + '.' + dc + '.list-manage.com/subscribe/post-json',
+        url = '//' + username + '.' + dc + '.list-manage.com/subscribe/post-json';
+        /*
         params = {
           name: tpl.$('input[name=name]').val(),
           email: tpl.$('input[name=email]').val(),
           c: 'JSON_CALLBACK',
           u: tpl.$('input[name=mailchimpAPI]').val(),
           id: tpl.$('input[name=mailchimpID]').val()
+        };*/
+
+    var params = {
+          name: tpl.$('input[name=name]').val(),
+          email: tpl.$('input[name=email]').val()
         };
 
     console.log(params);
--- server/methods/contents.js
+++ server/methods/contents.js
@@ -13,8 +13,31 @@ Meteor.methods({
       throw new Meteor.Error('no-email-input', 'Please enter your email');
     }
 
-    var test = console.log('test');
+    var formData = _.extend(_.pick(attributes, 'name', 'email'), {
+      c: 'JSON_CALLBACK',
+      u: '800bfb6e2a03c12de30b3eff1',
+      id: '9a5ebe0d08'
+    });
 
-    return test;
+    var submit = $.ajax({
+      type: 'POST',
+      url: '//damir.us11.list-manage.com/subscribe/post-json',
+      data: formData,
+      cache: false,
+      dataType: 'json',
+      contentType: 'application/json; charset=utf-8',
+      error: function(err) {
+        console.log('Could not connect to the registration server. Please try again later.');
+      },
+      success: function(data) {
+        if (data.result != 'success') {
+          console.log(data.msg);
+        } else {
+          console.log(data.msg);
+        }
+      }
+    });
+
+    return submit;
   }
 });

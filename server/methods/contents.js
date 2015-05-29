0f20698d862756ff54dd8129aafecc8d2ad925e2
Damir <damir@withpulp.com>
2015-05-29 15:44:02 -0400

    fix to method - returns http error

--- server/methods/contents.js
+++ server/methods/contents.js
@@ -25,9 +25,10 @@ Meteor.methods({
     try {
       var result = HTTP.call("//damir.us11.list-manage.com/subscribe/post-json",
         {data: formData});
-      return true;
+      return result;
     } catch (e) {
-      // Got a network error, time-out or HTTP error in the 400 or 500 range.
+      // Got a network error, time-out or HTTP error in the 400 or 500 range
+      throw new Meteor.Error('network-error', 'Something went wrong with the HTTP call to mailchimp');
       return false;
     }
   }

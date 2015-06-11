0edd0527c81773344c576af968f11aca8d954f67
Damir <damir@withpulp.com>
2015-06-11 16:25:08 -0400

    removed hidden fields from subscribe markup and event

--- client/views/widgets/subscribe.js
+++ client/views/widgets/subscribe.js
@@ -2,20 +2,10 @@ Template['subscribe'].events({
   'submit .subscribe.form': function(e, tpl) {
 		e.preventDefault();
 
-    // @TODO: move all this into server method for security
-    // remove hidden fields from form - pull from db
-
-    var username = tpl.$('input[name=mailchimpUsername]').val(),
-        dc = tpl.$('input[name=mailchimpDC]').val(),
-        url = '//' + username + '.' + dc + '.list-manage.com/subscribe/post-json';
-
     var params = {
           name: tpl.$('input[name=name]').val(),
           email: tpl.$('input[name=email]').val()
         };
-
-    console.log(params);
-    console.log(url);
 
     /*
     Meteor.call('submitSubscription', params, function(error) {
